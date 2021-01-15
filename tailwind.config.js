676e7348a4f09011718dab213d1cd111c4430640
alfinkerin <kerinalfin70@gmail.com>
2021-01-15 10:32:28 +0700

    update config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
